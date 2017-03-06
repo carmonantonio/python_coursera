de6c17f55f36df0569b3e4a08d7f8b6748475d2d
Antonio Carmona <carmonantonio@gmail.com>
2017-03-06 11:35:36 -0400

    another one

--- regex_assignment.py
+++ regex_assignment.py
@@ -1,1 +1,7 @@
 # this program takes a .txt file retrieve the numbers in it and sums it
+import re
+fname = input('Please enter a file name: ')
+handle =open(fname)
+numbers = list()
+for line in handle:
+    line = line.rstrip()
