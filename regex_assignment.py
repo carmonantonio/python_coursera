6a9e9fe3e4a1bbf1e5b686e6eaf68c3f58266e5e
Antonio Carmona <carmonantonio@gmail.com>
2017-03-06 13:22:46 -0400

    ahora si lo resolvi

--- regex_assignment.py
+++ regex_assignment.py
@@ -1,7 +1,16 @@
 # this program takes a .txt file retrieve the numbers in it and sums it
 import re
-fname = input('Please enter a file name: ')
+fname = 'regex_sum_353133.txt'
 handle =open(fname)
 numbers = list()
 for line in handle:
     line = line.rstrip()
+    x = re.findall(('[0-9]+'),line)
+    if len(x) > 0:
+        for y in x:
+            val = int(y)
+            numbers.append(val)
+print('There are:',len(numbers), 'numbers in the file')
+print(sum(numbers))
+
+
